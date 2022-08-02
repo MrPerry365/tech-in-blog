c06644d3bc167cee64f6f94e72e05e4df6d5f293
MrPerry365 <1987nowhere@gmail.com>
2022-08-02 17:04:59 -0400

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: '',
-  database: 'blogpost_db'
-});
+// require('dotenv').config();
 
-module.exports = db;
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
